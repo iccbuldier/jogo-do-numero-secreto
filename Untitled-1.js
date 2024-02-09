a2ba30fc46027c89e6f07ccc788ce8af42fb5c10
iccbuldier <159490514+iccbuldier@users.noreply.github.com>
2024-02-09 16:36:10 -0400

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,34 @@
+//tarefa 1
+let BoasVindas = `Ola Mundo`;// certo
+function apresntacaoBemVindo() { //Certo
+    console.log (BoasVindas); // Certo
+}
+
+function exibirOla() {
+    console.log ('olá, mundo');
+}
+
+exibirOla ();
+
+// tarefa 2
+function exibirNome(nome) {
+    console.log (`Olá ${nome}`);
+}
+exibirNome()
+
+//Tarefa 3
+function exibirNumero(numero) {
+    return numero * 2;
+}
+let resultadoDobro = exibirNumero ();
+console.log (resultadoDobro)
+
+//Tarefa 4
+function mediaNumeros(a, b, c) {
+    return (a, b, c)/3;
+}
+
+let media = mediaNumeros ();
+console.log (media)
+
+//Tarefa 5
